53bf8975365c335f4d2cbf1eb55d3e448d0d5801
romanilicev <shiftr123>
2019-11-24 18:45:34 +0300

    Хранение состояния в классе

--- tasks.js
+++ tasks.js
@@ -3,13 +3,56 @@ const onOpenCvReady = () => {
     console.log('Библиотека OpenCV.js загружена')
 };
 
+class Store {
+    store = {
+        hue: 0,
+        value: 0,
+        brightness: 0,
+        blur: 0,
+    };
+
+    saveValues(key, value) {
+        this.store[key] = Number(value);
+        this.displayWithChanges(this.store);
+    }
+
+    displayWithChanges(store) {
+        let image = cv.imread('canvasOutput_2');
+        // Смена яркости
+        image.convertTo(image, -1, 1, store.brightness);
+
+        // // блюр до преобразования
+        // let blur = new cv.Mat();
+        // let ksize = new cv.Size(store.blur, store.blur);
+        // cv.GaussianBlur(brightness, blur, ksize, 0, 0, cv.BORDER_DEFAULT);
+        // cv.imshow('canvasOutput_2_result', blur);
+
+        //
+        // // перевод в HSV пространство
+        cv.cvtColor(image, image, cv.COLOR_RGB2HSV);
+        for (let i = 0; i < image.matSize[1]; i++) {
+            for (let j = 0; j < image.matSize[0]; j++) {
+                image.ucharPtr(j, i)[0] = store.hue; // 0 канал из HSV
+                image.ucharPtr(j, i)[1] = store.value; // 1 канал из HSV
+            }
+        }
+        // // перевод в RGB пространство
+        cv.cvtColor(image, image, cv.COLOR_HSV2RGB);
+
+        cv.imshow('canvasOutput_2_result', image);
+        image.delete();
+        renderHistogram()
+    }
+}
+
+const store = new Store();
 const resetImgButton = document.getElementById('ResetImgButton');
 const imgElement = document.getElementById('imageSrc');
 
 resetImgButton.addEventListener('click', () => {
     let mat = cv.imread('canvasOutput_2');
     cv.imshow('canvasOutput_2_result', mat);
-    renderHistogram()
+    renderHistogram();
     mat.delete()
 });
 
@@ -22,7 +65,8 @@ setTimeout(() => {
     cv.medianBlur(src, dst, 1);
     cv.imshow('canvasOutput_2_result', dst);
     renderHistogram();
-    src.delete(); dst.delete();
+    src.delete();
+    dst.delete();
 }, 800);
 
 const BlurInputRangeEl = document.getElementById('blurInputRange');
@@ -31,59 +75,19 @@ const HueInputRangeEl = document.getElementById('HueInputRange');
 const ValueInputRangeEl = document.getElementById('ValueInputRange');
 
 HueInputRangeEl.addEventListener('input', (evt) => {
-    let image = cv.imread('canvasOutput_2_result');
-    cv.cvtColor(image, image, cv.COLOR_RGB2HSV);
-    // cv.cvtColor(image, image, cv.COLOR_RGB2BGR);
-    // cv.cvtColor(image, image, cv.COLOR_BRG2HSV);
-
-    for (let i = 0; i < image.matSize[1]; i++) {
-        for (let j = 0; j < image.matSize[0]; j++) {
-            image.ucharPtr(j, i)[0] = Number(evt.target.value); // 0 канал из HSV
-
-        }
-    }
-    cv.cvtColor(image, image, cv.COLOR_HSV2RGB);
-    cv.imshow('canvasOutput_2_result', image);
-    renderHistogram()
+    store.saveValues('hue', evt.target.value)
 });
 
 ValueInputRangeEl.addEventListener('input', (evt) => {
-    let image = cv.imread('canvasOutput_2_result');
-    cv.cvtColor(image, image, cv.COLOR_RGB2HSV);
-
-    for (let i = 0; i < image.matSize[1]; i++) {
-        for (let j = 0; j < image.matSize[0]; j++) {
-            image.ucharPtr(j, i)[1] = Number(evt.target.value); // 1 канал из HSV
-
-        }
-    }
-    cv.cvtColor(image, image, cv.COLOR_HSV2RGB);
-    cv.imshow('canvasOutput_2_result', image);
-    renderHistogram()
+    store.saveValues('value', evt.target.value);
 });
 
 BrightnessInputRangeEl.addEventListener('input', (evt) => {
-    let image = cv.imread('canvasOutput_2_result');
-    cv.cvtColor(image, image, cv.COLOR_RGB2HSV);
-
-    for (let i = 0; i < image.matSize[1]; i++) {
-        for (let j = 0; j < image.matSize[0]; j++) {
-            image.ucharPtr(j, i)[2] = Number(evt.target.value); // 2 канал из HSV
-        }
-    }
-    cv.cvtColor(image, image, cv.COLOR_HSV2RGB);
-    cv.imshow('canvasOutput_2_result', image);
-    renderHistogram()
+    store.saveValues('brightness', evt.target.value)
 });
 
 BlurInputRangeEl.addEventListener('input', (evt) => {
-    let src = cv.imread('canvasOutput_2_result');
-    let dst = new cv.Mat();
-// You can try more different parameters
-    cv.medianBlur(src, dst, Number(evt.target.value));
-    cv.imshow('canvasOutput_2_result', dst);
-    renderHistogram();
-    src.delete(); dst.delete();
+    store.saveValues('blur', evt.target.value)
 });
 
 const renderHistogram = () => {
@@ -113,6 +117,10 @@ const renderHistogram = () => {
         cv.rectangle(dst, point1, point2, color, cv.FILLED);
     }
     cv.imshow('histogram', dst);
-    src.delete(); dst.delete(); srcVec.delete(); mask.delete(); hist.delete();
+    src.delete();
+    dst.delete();
+    srcVec.delete();
+    mask.delete();
+    hist.delete();
 };
 
